189c9b3b0729cdfc4f894455da6580dd2af7958d
Maria Silvia <mariasrr97@gmail.com>
2022-08-21 19:33:12 -0300

    add cleanTable e paintTable

--- script.js
+++ script.js
@@ -20,8 +20,46 @@ $(document).ready(function () {
     SELECTED_STUDENT = STUDENTS[ra];
     console.log(SELECTED_STUDENT);
     $("#nome_aluno").text(SELECTED_STUDENT["NOME_ALUNO"]);
+    cleanTable();
     painTable();
   }
 
-  function painTable(params) {}
+  function painTable() {
+    const courses = SELECTED_STUDENT.grade;
+    $("td").each(function () {
+      const code = $(this).text();
+      if (courses[code]) {
+        console.log(courses[code].ultima_matricula.SITUACAO);
+        switch (courses[code].ultima_matricula.SITUACAO) {
+          case "Aprovado":
+          case "Dispensa de Disciplinas (com nota)":
+            $(this).addClass("aprovado");
+            break;
+
+          case "Reprovado por Frequência":
+          case "Reprovado por nota":
+            $(this).addClass("reprovado");
+            break;
+
+          case "Matrícula":
+            $(this).addClass("matriculado");
+            break;
+
+          default: // Cancelado, Trancamento Total
+            $(this).addClass("outro");
+            break;
+        }
+      }
+    });
+  }
+
+  function cleanTable() {
+    $("td").each(function () {
+      $(this).removeClass("aprovado");
+      $(this).removeClass("reprovado");
+      $(this).removeClass("matriculado");
+      $(this).removeClass("equivalencia");
+      $(this).removeClass("outro");
+    });
+  }
 });

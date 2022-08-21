8f5b14db8c1e4dbe2be956dbcc5e104ae69b938f
Maria Silvia <mariasrr97@gmail.com>
2022-08-21 16:53:02 -0300

    reorganize in alunos_xml_parsing.js

--- script.js
+++ script.js
@@ -1,80 +1,9 @@
 var SELECTED_STUDENT = null;
-var students = {};
-var data_json = [];
-loadDataAsJSON();
-
-function loadDataAsJSON(params) {
-  return $.ajax({
-    url: "alunos.xml",
-    dataType: "xml",
-    success: function (data) {
-      // Extract relevant data from XML
-      alunos_xml = $("ALUNOS_CURSO", data)[0].children;
-      for (let aluno of alunos_xml) {
-        let aluno_json = {};
-        for (let tag of aluno.children) {
-          aluno_json[tag.tagName] = tag.textContent;
-        }
-        data_json.push(aluno_json);
-      }
-      buildStudentsObject(data_json);
-    },
-    error: function (data) {
-      console.log("Error loading XML data");
-    },
-  });
-}
-
-function buildStudentsObject(data_json) {
-  data_json.forEach((data) => {
-    let grr = data["MATR_ALUNO"];
-    let grade;
-    if (!students[grr]) {
-      // novo aluno
-      students[grr] = {
-        NOME_ALUNO: data["NOME_ALUNO"],
-      };
-      grade = [];
-    } else grade = students[grr].grade; // pega grade que ja existe
-
-    let code = data["COD_ATIV_CURRIC"];
-    if (!grade[code]) {
-      // nova materia na grade
-      grade[code] = {
-        NOME_ATIV_CURRIC: data["NOME_ATIV_CURRIC"],
-        matriculas: [],
-      };
-    }
-    let matricula = {
-      MEDIA_FINAL: data["MEDIA_FINAL"],
-      FREQUENCIA: data["FREQUENCIA"],
-      SITUACAO: data["SITUACAO"],
-      ANO: data["ANO"],
-      PERIODO: data["PERIODO"],
-    };
-
-    // add matricula em matriculas da disciplina
-    grade[code].matriculas.push(matricula);
-
-    // update matricula mais recente
-    grade[code].ultima_matricula = getMaiorAnoSem(
-      grade[code].ultima_matricula,
-      matricula
-    );
-
-    // assign grade atualizada no grr
-    students[grr].grade = grade;
-  });
-}
-
-function getMaiorAnoSem(a, b) {
-  if (!a) return b;
-  if (a["ANO"] == b["ANO"]) return a["PERIODO"][0] == "2" ? a : b;
-  else return a["ANO"] > b["ANO"] ? a : b;
-}
+var STUDENTS = {};
+loadDataAsJSON(STUDENTS);
 
 $(document).ready(function () {
-  console.log("students", students);
+  console.log("students", STUDENTS);
 
   $("button").click(loadGRR);
   function loadGRR() {

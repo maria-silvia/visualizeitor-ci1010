8c79f4d4a8085886d9b188254f7347a38ab30a1a
Maria Silvia <mariasrr97@gmail.com>
2022-08-22 23:51:57 -0300

    make second popup

--- visualizeitor.js
+++ visualizeitor.js
@@ -11,15 +11,18 @@ $(document).ready(function () {
   $("#selectGRR").change(loadGRR);
 
   $("td").mousedown(function (event) {
-    if (event.which == 1) openLastMatricula(event);
-    else openHistorico(event);
+    closePopup();
+    let code = event.currentTarget.innerText;
+    if (event.which == 1) openLastMatricula(code);
+    else openHistorico(code);
   });
 
-  $("#closePopup").click(() => $(".popup").removeClass("active"));
+  $(".close").click(() => closePopup());
   $(document).keyup(function (e) {
-    if (e.key === "Escape") {
-      $(".popup").removeClass("active");
-    }
+    if (e.key === "Escape") closePopup();
+  });
+  $(document).contextmenu(function () {
+    return false;
   });
   // ===========================================================================
 
@@ -50,51 +53,59 @@ $(document).ready(function () {
       const code = $(this).text();
       if (courses[code]) {
         $(this).addClass("td_clicavel");
-        assignClassBySituation(this, courses[code].ultima_matricula.SITUACAO);
+        let c = getClassBySituation(courses[code].ultima_matricula.SITUACAO);
+        $(this).addClass(c);
       }
     });
   }
 
-  function openLastMatricula(event) {
-    let code = event.currentTarget.innerText;
+  function openLastMatricula(code) {
     if (SELECTED_STUDENT.grade[code]) {
       let name = SELECTED_STUDENT.grade[code].NOME_ATIV_CURRIC;
       $("#code_name").text(`${code} - ${name}`);
 
       let matr = SELECTED_STUDENT.grade[code].ultima_matricula;
-      $("#date").text(`${matr.ANO} / ${matr.PERIODO}`);
-      $("#grade").text(matr.MEDIA_FINAL);
-      $("#freq").text(matr.FREQUENCIA);
-      $("#situation").text(matr.SITUACAO);
-      cleanSituationClassFromElement(".status_tag");
-      assignClassBySituation(".status_tag", matr.SITUACAO);
+      let block = getMatriculaBlock(matr);
+      $("#popup-content").append(block);
+      $("#summary-popup").addClass("active");
+    }
+  }
+
+  function openHistorico(code) {
+    if (SELECTED_STUDENT.grade[code]) {
+      let name = SELECTED_STUDENT.grade[code].NOME_ATIV_CURRIC;
+      $("#code_name").text(`${code} - ${name}`);
+
+      SELECTED_STUDENT.grade[code].matriculas.forEach((matr) => {
+        let block = getMatriculaBlock(matr);
+        $("#popup-content").append(block);
+      });
 
       $("#summary-popup").addClass("active");
     }
   }
 
-  function openHistorico(event) {}
+  function closePopup() {
+    $(".popup").removeClass("active");
+    $("#popup-content").empty();
+  }
 
   // -------------------------------- HELPERS ----------------------------------
-  function assignClassBySituation(el, situation) {
+  function getClassBySituation(situation) {
     switch (situation) {
       case "Aprovado":
       case "Dispensa de Disciplinas (com nota)":
-        $(el).addClass("aprovado");
-        break;
+        return "aprovado";
 
       case "Reprovado por Frequência":
       case "Reprovado por nota":
-        $(el).addClass("reprovado");
-        break;
+        return "reprovado";
 
       case "Matrícula":
-        $(el).addClass("matriculado");
-        break;
+        return "matriculado";
 
       default: // Cancelado, Trancamento Total
-        $(el).addClass("outro");
-        break;
+        return "outro";
     }
   }
 
@@ -107,6 +118,24 @@ $(document).ready(function () {
       $(this).removeClass("td_clicavel");
       $(this).removeClass("outro");
     });
+  }
+
+  function getMatriculaBlock(matr) {
+    let situation_class = getClassBySituation(matr.SITUACAO);
+    return `<p class="status_tag ${situation_class}">
+          <span>${matr.SITUACAO}</span>
+          <span>${matr.ANO} / ${matr.PERIODO}</span>
+        </p>
+        <p class="info">
+          <span>
+            <strong>Nota: </strong>
+            <span>${matr.MEDIA_FINAL}</span>
+          </span>
+          <span>
+            <strong>Frequência: </strong>
+            <span>${matr.FREQUENCIA}</span>
+          </span>
+        </p>`;
   }
   // ===========================================================================
 });

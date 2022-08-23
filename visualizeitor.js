76d9db44d47a70a7113d3a18bf5a183707817950
Maria Silvia <mariasrr97@gmail.com>
2022-08-22 22:44:40 -0300

    improve popup styling, make helper funtions

--- visualizeitor.js
+++ visualizeitor.js
@@ -2,24 +2,26 @@ var STUDENTS = {};
 var SELECTED_STUDENT = null;
 
 $(document).ready(function () {
-  // ============= EVENT REGISTRATION ===============================
+  // ------------------ INICIALIZA -----------------
+  loadDataAsJSON(STUDENTS).then(() => {
+    fillSelect();
+  });
+
+  // ---------------------- EVENT REGISTRATION ---------------------------------
   $("#selectGRR").change(loadGRR);
 
   $("td").mousedown(function (event) {
     if (event.which == 1) openLastMatricula(event);
-    else openHistorico();
+    else openHistorico(event);
   });
 
-  $("#closePopup").click(() => $(".popup-right-click").removeClass("active"));
-
+  $("#closePopup").click(() => $(".popup").removeClass("active"));
   $(document).keyup(function (e) {
     if (e.key === "Escape") {
-      $(".popup-right-click").removeClass("active");
+      $(".popup").removeClass("active");
     }
   });
-  // ================================================================
-
-  loadDataAsJSON(STUDENTS).then(() => fillSelect()); // -- INICIALIZA
+  // ===========================================================================
 
   function fillSelect() {
     let GRRs = Object.keys(STUDENTS);
@@ -28,6 +30,7 @@ $(document).ready(function () {
         $("<option></option>").attr("value", value).text(value)
       );
     });
+
     // já inicia com o primeiro carregado
     SELECTED_STUDENT = GRRs[0];
     loadGRR();
@@ -37,47 +40,18 @@ $(document).ready(function () {
     let ra = $("#selectGRR").val();
     SELECTED_STUDENT = STUDENTS[ra];
     $("#nome_aluno").text(`Grade 2011 - ${SELECTED_STUDENT["NOME_ALUNO"]}`);
-    cleanTable();
     painTable();
   }
 
   function painTable() {
     const courses = SELECTED_STUDENT.grade;
+    cleanSituationClassFromElement("td");
     $("td").each(function () {
       const code = $(this).text();
       if (courses[code]) {
         $(this).addClass("td_clicavel");
-        switch (courses[code].ultima_matricula.SITUACAO) {
-          case "Aprovado":
-          case "Dispensa de Disciplinas (com nota)":
-            $(this).addClass("aprovado");
-            break;
-
-          case "Reprovado por Frequência":
-          case "Reprovado por nota":
-            $(this).addClass("reprovado");
-            break;
-
-          case "Matrícula":
-            $(this).addClass("matriculado");
-            break;
-
-          default: // Cancelado, Trancamento Total
-            $(this).addClass("outro");
-            break;
-        }
+        assignClassBySituation(this, courses[code].ultima_matricula.SITUACAO);
       }
-    });
-  }
-
-  function cleanTable() {
-    $("td").each(function () {
-      $(this).removeClass("aprovado");
-      $(this).removeClass("reprovado");
-      $(this).removeClass("matriculado");
-      $(this).removeClass("equivalencia");
-      $(this).removeClass("td_clicavel");
-      $(this).removeClass("outro");
     });
   }
 
@@ -86,12 +60,51 @@ $(document).ready(function () {
     if (SELECTED_STUDENT.grade[code]) {
       let name = SELECTED_STUDENT.grade[code].NOME_ATIV_CURRIC;
       $("#code_name").text(`${code} - ${name}`);
+
       let matr = SELECTED_STUDENT.grade[code].ultima_matricula;
       $("#date").text(`${matr.ANO} / ${matr.PERIODO}`);
       $("#grade").text(matr.MEDIA_FINAL);
       $("#freq").text(matr.FREQUENCIA);
       $("#situation").text(matr.SITUACAO);
-      $(".popup-right-click").addClass("active");
+      cleanSituationClassFromElement("#situation");
+      assignClassBySituation("#situation", matr.SITUACAO);
+
+      $("#summary-popup").addClass("active");
     }
   }
+
+  // -------------------------------- HELPERS ----------------------------------
+  function assignClassBySituation(el, situation) {
+    switch (situation) {
+      case "Aprovado":
+      case "Dispensa de Disciplinas (com nota)":
+        $(el).addClass("aprovado");
+        break;
+
+      case "Reprovado por Frequência":
+      case "Reprovado por nota":
+        $(el).addClass("reprovado");
+        break;
+
+      case "Matrícula":
+        $(el).addClass("matriculado");
+        break;
+
+      default: // Cancelado, Trancamento Total
+        $(el).addClass("outro");
+        break;
+    }
+  }
+
+  function cleanSituationClassFromElement(element) {
+    $(element).each(function () {
+      $(this).removeClass("aprovado");
+      $(this).removeClass("reprovado");
+      $(this).removeClass("matriculado");
+      $(this).removeClass("equivalencia");
+      $(this).removeClass("td_clicavel");
+      $(this).removeClass("outro");
+    });
+  }
+  // ===========================================================================
 });
